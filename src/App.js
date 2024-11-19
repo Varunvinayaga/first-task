5dc1cbf907ae2fce9d5bf98b496d0a927c5a0d5a
varun <varunvinayaga692@gmail.com>
2024-11-19 15:09:28 +0530

    task added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>welcome react js</h1>
+       <h1>Hi am varun you are 23years old</h1>
+        </header>
+    </div>
+  );
+}
+export default App;   
